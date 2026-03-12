b74d06d01be061d9011b9ca7d2bd9ab9f659109e
Blaine Stidham <blaine.stidha19@gmail.com>
2026-03-12 13:58:47 -0400

    Added NameController and Welcome view

---
+++ VersionControlDemo/Controllers/NameController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VersionControlDemo.Controllers
+{
+    public class NameController : Controller
+    {
+        public IActionResult Welcome(string name)
+        {
+            ViewBag.Name = name;
+            return View();
+        }
+    }
+}
