0c0954e544f5c17ea867cc2d45aa3bbf2d825011
blain <blain@BLAINE>
2026-03-12 13:40:11 -0400

    Add project files.

---
+++ VersionControlDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VersionControlDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
